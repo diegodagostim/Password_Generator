01a2b4774b96ea8740a1108dafbe8992d63f2c73
diegodagostim <44005689+diegodagostim@users.noreply.github.com>
2023-01-14 19:49:29 +0000

    function generate password added.

--- script.js
+++ script.js
@@ -1,118 +1,163 @@
 // Array of special characters to be included in password
 var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
   "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
 
+/*
 // Function to prompt user for password options
-function getPasswordOptions() {
-
-}
-
+function getPasswordOptions() {}
+getPasswordOptions();
 // Function for getting a random element from an array
-function getRandom(arr) {
-
-}
+function getRandom(arr) {}
 
 // Function to generate password with user input
-function generatePassword() {
-
-}
+function generatePassword() {}
 
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener("click", writePassword);
+*/
+
+/*
+const key_strings = {
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  number: "0123456789",
+  symbol: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+};
+*/
+
+function generatePassword() {
+  var passwordCharSet = "";
+
+  var length = window.prompt(
+    "Enter a number from 8 to 128 for password length."
+  );
+
+  var lowerCasedCharacters = window.confirm(
+    "Would you like to use lowercase letters?"
+  );
+  if (lowerCasedCharacters) {
+    passwordCharSet += key_strings.lowerCasedCharacters;
+  }
+
+  var upperCasedCharacters = window.confirm(
+    "Would you like to use uppercase letters?"
+  );
+  if (upperCasedCharacters) {
+    passwordCharSet += key_strings.upperCasedCharacters;
+  }
+
+  var specialCharacters = window.confirm("Would you like to use symbols?");
+  if (specialCharacters) {
+    passwordCharSet += key_strings.specialCharacters;
+  }
+
+  var numericCharacters = window.confirm("Would you like to use numbers?");
+  if (numericCharacters) {
+    passwordCharSet += key_strings.numericCharacters;
+  }
+  var password = "";
+  for (let i = 0; i < length; i++) {
+    password +=
+      passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)];
+  }
+  return password;
+}
+
+console.log(generatePassword());
